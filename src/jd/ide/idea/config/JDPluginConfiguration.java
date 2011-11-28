beb08f5a4741a0eb8aca0e5413ff7a3e87e76257
Brice Dutheil <803621+bric3@users.noreply.github.com>
2011-11-28 17:35:22 +0100

    Removed the switch to active/inactive the plugin while running, it doesn't work really well do to the IntelliJ cache, need some more digging before adding it back

--- src/jd/ide/idea/config/JDPluginConfiguration.java
+++ src/jd/ide/idea/config/JDPluginConfiguration.java
@@ -9,25 +9,21 @@ public class JDPluginConfiguration {
     protected static final String COMPONENT_NAME = "JDPluginConfiguration";
     private JCheckBox displayLineNumbersCheckBox;
     private JCheckBox displayMetadataCheckBox;
-    private JCheckBox enableJavaDecompilerCheckBox;
     private JPanel contentPane;
     private JTextPane usingJDCore0TextPane;
 
     public void storeDataTo(JDPluginComponent jdPluginComponent) {
-        jdPluginComponent.setPluginEnabled(enableJavaDecompilerCheckBox.isSelected());
         jdPluginComponent.setDisplayLineNumbersEnabled(displayLineNumbersCheckBox.isSelected());
         jdPluginComponent.setDisplayMetadataEnabled(displayMetadataCheckBox.isSelected());
     }
 
     public void readDataFrom(JDPluginComponent jdPluginComponent) {
-        enableJavaDecompilerCheckBox.setSelected(jdPluginComponent.isPluginEnabled());
         displayLineNumbersCheckBox.setSelected(jdPluginComponent.isDisplayLineNumbersEnabled());
         displayMetadataCheckBox.setSelected(jdPluginComponent.isDisplayMetadataEnabled());
     }
 
     public boolean isModified(JDPluginComponent jdPluginComponent) {
-        return  enableJavaDecompilerCheckBox.isSelected() != jdPluginComponent.isPluginEnabled()
-                || displayLineNumbersCheckBox.isSelected() != jdPluginComponent.isDisplayLineNumbersEnabled()
+        return displayLineNumbersCheckBox.isSelected() != jdPluginComponent.isDisplayLineNumbersEnabled()
                 || displayMetadataCheckBox.isSelected() != jdPluginComponent.isDisplayMetadataEnabled();
     }
 
