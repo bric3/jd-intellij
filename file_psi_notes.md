775030ffb41838eeededb069b1c262466dfc9f37
Brice Dutheil <803621+bric3@users.noreply.github.com>
2013-10-08 21:21:12 +0200

    Another tryout based on ClsCustomNavigationPolicy, but unsuccessful at the moment

---
+++ file_psi_notes.md
@@ -0,0 +1,19 @@
+
+
+Kotlin plugin
+=============
+https://github.com/JetBrains/kotlin/blob/master/idea/src/META-INF/plugin.xml
+https://github.com/JetBrains/kotlin/blob/master/idea/src/org/jetbrains/jet/plugin/libraries/JetDecompiledData.java
+
+
+~April 2013 ClsFileImpl of IntelliJ
+===================================
+https://github.com/JetBrains/intellij-community/blob/e0387ede2acc49a319dcbf7ef2655380ea810e8e/java/java-psi-impl/src/com/intellij/psi/impl/compiled/ClsFileImpl.java
+
+
+
+Erlang plugin
+=============
+https://github.com/ignatov/intellij-erlang/blob/master/src/META-INF/plugin.xml
+https://github.com/ignatov/intellij-erlang/blob/master/src/org/intellij/erlang/ErlangStructureViewFactory.java
+
--- src/main/java/jd/ide/intellij/JavaClassDecompiledPsiFileProvider.java
+++ src/main/java/jd/ide/intellij/JavaClassDecompiledPsiFileProvider.java
@@ -1,8 +1,14 @@
 package jd.ide.intellij;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.progress.NonCancelableSection;
 import com.intellij.openapi.progress.ProgressIndicatorProvider;
+import com.intellij.openapi.util.Key;
 import com.intellij.psi.ClsFileDecompiledPsiFileProvider;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiFileFactory;
@@ -11,8 +17,6 @@ import com.intellij.psi.impl.compiled.ClsFileImpl;
 import com.intellij.psi.impl.source.SourceTreeToPsiMap;
 import com.intellij.psi.impl.source.tree.TreeElement;
 import com.intellij.psi.util.PsiUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * Provider extension that builds a {@link com.intellij.psi.PsiFile} from the JD decompiled text, otherwise IDEA uses
@@ -20,9 +24,11 @@ import org.jetbrains.annotations.Nullable;
  * references.
  *
  * @see <a href="http://youtrack.jetbrains.com/issue/IDEA-100029">http://youtrack.jetbrains.com/issue/IDEA-100029</a>
- *
  */
 public class JavaClassDecompiledPsiFileProvider implements ClsFileDecompiledPsiFileProvider {
+    private static Logger LOGGER = Logger.getInstance(JavaClassDecompiledPsiFileProvider.class);
+
+
     private final CachingJavaDecompilerService javaDecompilerService;
 
     public JavaClassDecompiledPsiFileProvider() {
@@ -30,12 +36,12 @@ public class JavaClassDecompiledPsiFileProvider implements ClsFileDecompiledPsiF
     }
 
 
-
     @Nullable
     @Override
     public PsiFile getDecompiledPsiFile(@NotNull PsiJavaFile clsFile) {
-            String decompiledText = javaDecompilerService.decompile(clsFile);
+        String decompiledText = javaDecompilerService.decompile(clsFile);
 
+        // investigate caching, called a lot
         PsiFile fileFromDecompiledText = PsiFileFactory.getInstance(clsFile.getProject()).createFileFromText(
                 clsFile.getName(),
                 clsFile.getLanguage(),
@@ -45,20 +51,27 @@ public class JavaClassDecompiledPsiFileProvider implements ClsFileDecompiledPsiF
         );
         fileFromDecompiledText.putUserData(PsiUtil.FILE_LANGUAGE_LEVEL_KEY, clsFile.getLanguageLevel());
 
+        LOGGER.info("Decompiled PsiFile of PsiJavaFile:'" + clsFile.getName() + "', instance:'" + System.identityHashCode(clsFile) + "'");
+        LOGGER.info("Decompiled PsiFile:'" + fileFromDecompiledText.getName() + "', instance:'" + System.identityHashCode(fileFromDecompiledText) + "'");
 
         // code taken from ClsFileImpl#getMirror()
-        TreeElement mirrorTreeElement = SourceTreeToPsiMap.psiToTreeNotNull(fileFromDecompiledText);
+        {
+            TreeElement mirrorTreeElement = SourceTreeToPsiMap.psiToTreeNotNull(fileFromDecompiledText);
 
-        if (clsFile instanceof ClsFileImpl) {
+            if (clsFile instanceof ClsFileImpl) {
                 NonCancelableSection section = ProgressIndicatorProvider.getInstance().startNonCancelableSection();
-            try {
-                ((ClsFileImpl) clsFile).setMirror(mirrorTreeElement);
-            }
-            finally {
-                section.done();
+                try {
+                    // TODO this code should be removed after 11.1 release. It is left just in case.
+                    // Document is not actually used, maybe it is stored in mirror just to avoid garbage collecting
+                    Document document = FileDocumentManager.getInstance().getDocument(clsFile.getVirtualFile());
+                    mirrorTreeElement.putUserData(Key.create("DOCUMENT_IN_MIRROR_KEY"), document);
+
+                    ((ClsFileImpl) clsFile).setMirror(mirrorTreeElement);
+                } finally {
+                    section.done();
+                }
             }
         }
-
 
         return fileFromDecompiledText;
     }
